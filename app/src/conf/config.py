a439b982ce69499505b5d5dd9d4076008a3c7eac
Ivan-Danyleiko <vanyadanuleyko@gmail.com>
2024-09-23 11:27:36 +0300

    Merge remote changes

--- app/src/conf/config.py
+++ app/src/conf/config.py
@@ -8,15 +8,21 @@ class Settings(BaseSettings):
     sqlalchemy_database_url: str
     secret_key: str
     algorithm: str
-    mail_username: str | None = None
-    mail_password: str | None = None
-    mail_from: str | None = None
-    mail_from_name: str | None = None
-    mail_port: int | None = None
-    mail_server: str | None = None
-    cors_origins: str | None = None
+    mail_username: str
+    mail_password: str
+    mail_from: str
+    mail_from_name: str
+    mail_port: int
+    mail_server: str
+    cors_origins: str
     # rate_limiter_times: int
     # rate_limiter_seconds: int
+
+    @validator('mail_port', pre=True, always=True)
+    def check_mail_port(cls, value):
+        if not isinstance(value, int):
+            raise ValueError("MAIL_PORT must be an integer")
+        return value
 
     model_config = ConfigDict(extra='ignore', env_file=env_file if env_file.exists() else None, env_file_encoding = "utf-8")
 
